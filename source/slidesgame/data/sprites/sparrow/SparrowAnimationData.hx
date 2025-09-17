6e98aa8f390541f746029ed728e95f814299a43a
Sphis-Sinco <sinconsistencia@gmail.com>
2025-09-17 08:49:19 -0500

    [sparrow-sprites-json] sparrow sprite data + sparrow animation data

---
+++ source/slidesgame/data/sprites/sparrow/SparrowAnimationData.hx
@@ -0,0 +1,7 @@
+package slidesgame.data.sprites.sparrow;
+
+class SparrowAnimationData {
+        public var name:String;
+        public var symbolname:String;
+        public var offsets:Array<Float>;
+}
---
+++ source/slidesgame/data/sprites/sparrow/SparrowSpriteData.hx
@@ -0,0 +1,16 @@
+package slidesgame.data.sprites;
+
+class SparrowSpriteData
+{
+        public var imageName:String;
+
+        public var animations:Array<SparrowAnimationData>;
+        
+        @:jignored
+	public var id:String;
+
+        public function new(id:String) {
+
+        }
+        
+}
