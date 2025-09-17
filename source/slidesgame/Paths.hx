2292ce5d2b506049162c934c6758a71e6500f2f9
Sphis-Sinco <sinconsistencia@gmail.com>
2025-09-17 08:53:53 -0500

    [sparrow-sprites-json] paths.hx

---
+++ source/slidesgame/Paths.hx
@@ -0,0 +1,40 @@
+package ontime;
+
+import lime.utils.Assets;
+#if sys
+import sys.io.File;
+#end
+
+class Paths
+{
+	public static function getAssetsPath(path:String)
+	{
+		var return_path_prefix:String = "";
+
+		if (!StringTools.startsWith(path, "assets/"))
+		{
+			return_path_prefix = "assets/";
+		}
+
+		return return_path_prefix + path;
+	}
+
+	public static function getDataFile(folder:String, file:String)
+	{
+		return getAssetsPath((folder != null ? folder + "/" : "") + "data/" + file));
+	}
+
+	public static function getImageFile(folder:String, file:String)
+	{
+		return getAssetsPath((folder != null ? folder + "/" : "") + "images/" + file+".png"));
+	}
+
+	public static function getText(path:String):String
+	{
+		#if sys
+		return File.getContent(path);
+		#end
+
+		return Assets.getText(path);
+	}
+}
